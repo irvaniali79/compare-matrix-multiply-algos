04a8e30a0b52ac1fb6762d24e55e53ef31bef083
Ali irvani <66170668+okabe-rintaru@users.noreply.github.com>
2021-06-20 22:35:21 +0430

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
  git push -f git@github.com:
-okabe-rintaru/heap-Sorts-Comparison.git master:gh-pages
+okabe-rintaru/compare-matrix-multiply-algos.git master:gh-pages
 
 cd -
